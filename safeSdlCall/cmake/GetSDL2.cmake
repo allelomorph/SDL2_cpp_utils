b478226240309719a88372ac79518cd4be411ec9
allelomorph <samuel.pomeroy@atlasschool.com>
2024-07-27 01:20:32 +0000

    generalize use of common scripts to external repo cmake_utils

--- safeSdlCall/cmake/GetSDL2.cmake
+++ safeSdlCall/cmake/GetSDL2.cmake
@@ -1,24 +1,35 @@
 # newest features used: FetchContent v3.11, FetchContent_MakeAvailable v3.14
-# targeting cmake v3.16
-cmake_minimum_required(VERSION 3.14)
+cmake_minimum_required(VERSION 3.16)
 
-# cmake 3.16-supplied FindSDL*.cmake modules are for SDL1.x, _not_ SDL2!
-include(LocatePackageOrFetchContent)
+# cmake 3.16-supplied FindSDL*.cmake modules are for SDL1.x, so to use SDL2 we
+#   need to supply our own find modules
 
-# !!! note option to set FETCH_ALL_DEPENDENCIES to OFF for testing
+include(FetchIfNotFound)
 
-# TBD add unset of FP_OPTIONS to prevent accidental use of unknown strings
+# See https://packages.ubuntu.com/search?keywords=sdl2 for package versions
+# In all cases currently only considering SDL2 v2, not v3
 
-# SDL2 release installed by local package manager and used in testing, but
-#   does not set targets SDL2::SDL2 and SDL2::SDL2main:
+# Relevant SDL2 releases:
+# - release matching Ubuntu 20.04 LTS package:
 #   2.0.10 0e9560aea22818884921e5e5064953257bfe7fa7
-# earliest SDL2 release to define targets SDL2::SDL2 and SDL2::SDL2main:
+# - earliest release to define targets SDL2::SDL2 and SDL2::SDL2main,
+#   as well as define SDL_islower and SDL_isalpha for SDL_rtf 2.0.0
 #   2.0.16 25f9ed87ff6947d9576fc9d79dee0784e638ac58
-# SDL2 current release at time of writing:
-#   2.28.3 8a5ba43d00252c6c8b33c9aa4f1048222955ab4d
+# - release matching Ubuntu 22.04 LTS package:
+#   2.0.20 b424665e0899769b200231ba943353a5fee1b6b6
+# - current release at last script update:
+#   2.28.5 15ead9a40d09a1eb9972215cceac2bf29c9b77f6
+set(FP_OPTIONS
+  2.0.16
+  )
+# CMAKE_CACHE_ARGS not passed by FetchContent to ExternalProject_Add as documented in:
+#   https://cmake.org/cmake/help/v3.16/module/FetchContent.html#command:fetchcontent_declare
+#   https://cmake.org/cmake/help/v3.16/module/ExternalProject.html#command:externalproject_add
+# So we set the cache variable directly instead of passing with `-D`, see:
+#   https://discourse.cmake.org/t/fetchcontent-cache-variables/1538
+set(SDL_STATIC OFF CACHE BOOL "Toggles building of SDL2 static library")
 set(FC_OPTIONS
   GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-  # 2.0.16
-  GIT_TAG        25f9ed87ff6947d9576fc9d79dee0784e638ac58
+  GIT_TAG        25f9ed87ff6947d9576fc9d79dee0784e638ac58 # 2.0.16
   )
-LocatePackageOrFetchContent(SDL2 "${FP_OPTIONS}" "${FC_OPTIONS}")
+fetch_if_not_found(SDL2 "${FP_OPTIONS}" "${FC_OPTIONS}")
