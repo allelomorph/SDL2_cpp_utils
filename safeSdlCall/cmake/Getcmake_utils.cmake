aef05d72502e0114ff406de5822b8a099a599450
allelomorph <samuel.pomeroy@atlasschool.com>
2024-07-29 22:18:43 +0000

    moved GetSDL2* scripts to dependency cmake_utils

--- safeSdlCall/cmake/Getcmake_utils.cmake
+++ safeSdlCall/cmake/Getcmake_utils.cmake
@@ -8,7 +8,7 @@ if(NOT cmake_utils_POPULATED)
     GIT_REPOSITORY https://github.com/allelomorph/cmake_utils.git
     # ExternalProject_Add defaults to origin/master up to at least cmake 3.30, see:
     #   - https://cmake.org/cmake/help/v3.30/module/ExternalProject.html#git
-    GIT_TAG        bc71e46037eccc54a94efb5658a36a9eb0d666f2  # origin/main
+    GIT_TAG        159bb9cb607e8e26c98ceb1ad95f7d9e9ca2a61f  # origin/main
   )
   FetchContent_MakeAvailable(cmake_utils)
   list(APPEND CMAKE_MODULE_PATH ${cmake_utils_SOURCE_DIR})
