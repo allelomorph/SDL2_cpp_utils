8e663c6a790c1cd53844d912ece7b972551d1ba8
allelomorph <samuel.pomeroy@atlasschool.com>
2025-02-09 07:18:18 +0000

    now always fetching current version of cmake_utils

--- sdl2_smart_ptrs/cmake/Getcmake_utils.cmake
+++ sdl2_smart_ptrs/cmake/Getcmake_utils.cmake
@@ -12,7 +12,7 @@ if(NOT cmake_utils_POPULATED)
     GIT_REPOSITORY https://github.com/allelomorph/cmake_utils.git
     # ExternalProject_Add defaults to origin/master up to at least cmake 3.30, see:
     #   - https://cmake.org/cmake/help/v3.30/module/ExternalProject.html#git
-    GIT_TAG        4789565a240d301c185b2413a8e5c19aeb3b3257  # origin/main
+    GIT_TAG        main  # origin/main
   )
   FetchContent_MakeAvailable(cmake_utils)
   list(APPEND CMAKE_MODULE_PATH ${cmake_utils_SOURCE_DIR})
